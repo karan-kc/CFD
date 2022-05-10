2499b008a76a4cfd5ac307c5f61a07c68d5c1af3
karan-kc <karankc@me.com>
2022-05-10 09:19:09 +0200

    Done with Aufgabe2

--- Rechnerubung/Week1/Aufgabe2/Aufgabe2.sh
+++ Rechnerubung/Week1/Aufgabe2/Aufgabe2.sh
@@ -1,4 +1,27 @@
-#!/bin/sh
+#!/bin/bash
+
+#For time file:
+cat $1 | grep "Time" | sed -n 2~2p | head -n  -2 | cut -d " " -f3 > time
+#Outputs 4621 lines
+
+#For maxCourantNumber.txt file:
+cat $1 | grep "Courant" | cut -d ":" -f3 > maxCourantNumber.txt
+
+#For meanFlowVelocity.txt file:
+cat $1 | grep "volAverage() of U" | cut -d "(" -f3 | cut -d " " -f1 | sed '1i\\' > meanFlowVelocity.txt
+
+#For forcePressure.txt file:
+cat $1 | grep "sum of forces" -A 1 | grep "pressure" | cut -d "(" -f2 | cut -d " " -f1 | sed '1i\\' > forcePressure.txt
+#or
+#cat $1 | grep -E "pressure : \(0" | cut -d "(" -f2 | cut -d " " -f1 | sed '1i\\' > forcePressure.txt
+
+#For forceViscous.txt file:
+cat $1 | grep "sum of forces" -A 2 | grep "viscous" | cut -d "(" -f2 | cut -d " " -f1 | sed '1i\\' > forceViscous.txt
+#or
+#cat $1 | grep -E "viscous  : \(0" | cut -d "(" -f2 | cut -d " " -f1 | sed '1i\\' > forceViscous.txt
+
+#Combines outputs into single file
+paste time maxCourantNumber.txt meanFlowVelocity.txt forcePressure.txt forceViscous.txt>combinedResult.txt
 
 gnuplot -p << EOF
 set terminal wxt 0	#Plot 0
