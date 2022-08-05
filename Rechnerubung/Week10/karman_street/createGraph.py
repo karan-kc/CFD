51aff2692d7c854aa816a702e0327f82cfb20bca
karan-kc <karankc@me.com>
2022-08-06 00:02:13 +0200

    Done with Week10 and final commit :)

--- Rechnerubung/Week10/karman_street/createGraph.py
+++ Rechnerubung/Week10/karman_street/createGraph.py
@@ -7,10 +7,12 @@ Created on Sat June 11 15:15:00 2022
 """
 import matplotlib.pyplot as mpl
 
+
 class simData(object):
     pass
 
-def readresults():
+
+def readandplotresults():
     """
     This function reads the output file of a simulation
     """
@@ -18,29 +20,41 @@ def readresults():
     data = simData()
     data.cd = []
     data.cl = []
+    data.tiempo = []
 
     notLine = ''
     for notLine in open('./postProcessing/forceCoeffs_object/0/forceCoeffs.dat','r'):
         li=notLine.strip()
         if not li.startswith("#"):
             line = notLine.rstrip()
+            data.tiempo.append(float(line.split("\t")[0]))
             data.cd.append(float(line.split("\t")[2]))
             data.cl.append(float(line.split("\t")[3]))
-    mpl.figure(num=1, dpi=500)
-    mpl.plot(data.cd, data.cl, 'g-', label='Lilienthal Polar')
-    mpl.xlabel('$c_{d}, [-]$')
-    mpl.ylabel('$c_{l}, [-]$')
-    # Legende aktivieren
-    mpl.legend()
+
+
+    fig, ax1 = mpl.subplots()
+    mpl.suptitle('$c_{l}$' + ' and ' + '$c_{d}$' + ' against time')
+    ax2 = ax1.twinx()
+    a, =  ax1.plot(data.tiempo, data.cl, color='blue', label='$c_{l}$')
+    b, = ax2.plot(data.tiempo, data.cd, color='green', label='$c_{d}$')
+    p = [a, b]
+    ax1.legend(p, [p_.get_label() for p_ in p],
+               loc= 'lower right', fontsize= 'small')
+
+    ax1.set_xlabel('Time, [s]')
+    ax1.set_ylabel('$c_{l}, [-]$')
+    ax2.set_ylabel('$c_{d}, [-]$')
+
     # Abspeichern als PNG
     mpl.tight_layout()
-    mpl.savefig('./plot.png', format="png")
+    mpl.savefig('./drag_coefficient_plot.png', format="png", dpi=500)
+
 
 def main():
     """
     this is the function used to call all functions necessary for the task
     """
-    data = readresults()
+    readandplotresults()
 
 if __name__ == "__main__":
     main()
